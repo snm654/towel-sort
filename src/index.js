8526a86d3d94e3ddaa1c16133b68935ca22dfc05
snm654 <ruengermany@yandex.by>
2022-08-05 18:27:59 +0300

    ia okazalsia ymnee etix testov

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+    const result = [];
+    if (matrix.length > 1) matrix[1].reverse()
+    if (matrix.length > 3) matrix[3].reverse()
+    for (let el of matrix) {
+        if (Array.isArray(el)) {
+            for (let subElement of el) {
+                result.push(subElement);
+            }
+        } else result.push(el);
+
+    }
+    return result
 }
+
+
+console.log(module.exports())
+
+
